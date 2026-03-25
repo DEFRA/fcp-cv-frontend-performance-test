71cdc8d82aade5d648043c179f0aabb519b4d993
gmarshd <gordon.marsh@defra.gov.uk>
2026-03-25 16:28:24 +0000

    Initial version

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,27 @@ JM_SCENARIOS=${JM_HOME}/scenarios
 JM_REPORTS=${JM_HOME}/reports
 JM_LOGS=${JM_HOME}/logs
 
+# Get an auth token
+auth_url=https://login.microsoftonline.com/${TENANT_ID:?required secret not set!}/oauth2/v2.0/token
+client_auth=`echo -n "${CLIENT_ID:?required secret not set!}:${CLIENT_SECRET:?required secret not set!}" | base64  | tr -d '\n'`
+
+content=`curl -s \
+  --connect-timeout 5 \
+  -x ${HTTP_PROXY:?required env var not set!} \
+  -L ${auth_url} \
+  -H "Authorization: Basic ${client_auth}" \
+  -H 'content-type: application/x-www-form-urlencoded' \
+  --data 'grant_type=client_credentials' \
+  --data "scope=${CLIENT_SCOPE:?required secret not set!}"`
+auth_token=$( jq -r  '.access_token' <<< "${content}" )
+identity_token=$( jq -r  '.id_token' <<< "${content}" )
+
+# fast-fail when no token available!
+if [ -z "${auth_token}" ] ; then
+  echo ERROR! Exiting because an auth token could not be retrieved
+  exit 2
+fi
+
 mkdir -p ${JM_REPORTS} ${JM_LOGS}
 
 TEST_SCENARIO=${TEST_SCENARIO:-test}
@@ -30,6 +51,8 @@ SERVICE_URL_SCHEME=${SERVICE_URL_SCHEME:-https}
 # Run the test suite
 jmeter -n -t ${SCENARIOFILE} -e -l "${REPORTFILE}" -o ${JM_REPORTS} -j ${LOGFILE} -f \
 -Jenv="${ENVIRONMENT}" \
+-JauthToken="${auth_token}" \
+-JidentityToken="${identity_token}" \
 -Jdomain="${SERVICE_ENDPOINT}" \
 -Jport="${SERVICE_PORT}" \
 -Jprotocol="${SERVICE_URL_SCHEME}"
